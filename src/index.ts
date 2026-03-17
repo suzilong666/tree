0330c3ee47c6c15f77039ef2c6d0c22ee6d1387a
suzilong <1500116008@qq.com>
2026-03-17 10:31:14 +0800

    feat:get模块改名query

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as traverse from './traverse/index'
 import * as find from './find/index'
 import * as transform from './transform/index'
-import * as get from './get/index'
+import * as query from './query/index'
 
 const tree = {
     ...traverse,
     ...find,
     ...transform,
-    ...get,
+    ...query,
 }
 
 export default tree
